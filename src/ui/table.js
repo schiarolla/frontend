bcea6103c0db14ab19f74f89d0be33ace83161a6
Shaun Chiarolla <schiarolla@gmail.com>
2021-05-05 08:18:02 -0700

    updated

--- src/ui/table.js
+++ src/ui/table.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import MaterialTable from 'material-table'
+import { Link } from '@material-ui/core'
+
+const management = {
+    'hpe': 'https://oneview.mscpec.com',
+    'hp': 'https://oneview.mscpec.com',
+    'lenovo': 'https://xclarity.mscpec.com',
+    'dell inc.': 'https://openmanage.mscpec.com',
+}
 
 const columns = [
     { title: 'Building', field: 'building', defaultGroupOrder: 0 },
     { title: 'Room', field: 'room', defaultGroupOrder: 1 },
-    { title: 'Rack', field: 'rack', defaultGroupOrder: 2 },
+    { title: 'Rack', field: 'rack', defaultGroupOrder: 2, cellStyle: { textTransform: 'capitalize' } },
     { title: 'Hostname', field: 'hostname' },
-    { title: 'IP', field: 'ip' },
-    { title: 'Manufacturer', field: 'manufacturer' },
+    { title: 'IP', field: 'ip', disableClick: true, render: row => <Link color='inherit' href={`https://${row.ip}`} target='_blank'>{row.ip}</Link> },
+    { title: 'Manufacturer', field: 'manufacturer', disableClick: true, render: row => <Link color='inherit' href={management[row.manufacturer]} target='_blank'>{row.manufacturer}</Link> },
     { title: 'Model', field: 'model' },
+    { title: 'Power', field: 'power', render: row => <div style={row.power === 'on' ? { color: 'green' } : { color: 'red' }}>{row.power}</div> },
+    { title: 'Health', field: 'health', render: row => <div style={row.health === 'ok' ? { color: 'green' } : { color: 'red' }}>{row.health}</div> },
 ]
 
 function Table({ data, handleClick }) {
@@ -17,7 +27,6 @@ function Table({ data, handleClick }) {
             <MaterialTable
                 columns={columns}
                 data={data || undefined}
-                title='bmc_scan_data'
                 actions={[
                     { icon: 'refresh', tooltip: 'Refresh BMC Data', isFreeAction: true, onClick: handleClick }
                 ]}
@@ -27,15 +36,16 @@ function Table({ data, handleClick }) {
                         overflow: 'hidden',
                         whiteSpace: 'nowrap'
                     },
+
                     paging: false,
                     showTitle: false,
                     padding: 'dense',
                     toolbarButtonAlignment: 'left',
-                    thirdSortClick: false,
-                    exportButton: true
+                    thirdSortClick: false
                 }}
+                onRowClick={(event, rowData, togglePanel) => togglePanel()}
                 detailPanel={row => (
-                    <div style={{ padding: 25, marginLeft: 'calc(100vw / 4)' }}>
+                    <div style={{ padding: 25, marginLeft: 275 }}>
                         <div>
                             <b>Sockets:   </b>
                             {row.cpu_count}
@@ -47,10 +57,6 @@ function Table({ data, handleClick }) {
                         <div>
                             <b>Asset Tag:   </b>
                             {row.asset_tag.toUpperCase()}
-                        </div>
-                        <div>
-                            <b>Health:   </b>
-                            {row.health.toUpperCase()}
                         </div>
                         <div>
                             <b>Guid:   </b>{row.guid.toUpperCase()}
